da7e704eb0cded2c2fcb93034649491888329b4e
L.D <2450600633@qq.com>
2018-09-03 20:42:13 +0800

    add

---
+++ app/src/main/java/biz/ldlong/com/xgpush/bean/TagTokenPair.java
@@ -0,0 +1,11 @@
+package biz.ldlong.com.xgpush.bean;
+
+public class TagTokenPair {
+    public TagTokenPair(String tag, String token) {
+        this.tag = tag;
+        this.token = token;
+    }
+
+    public String tag;
+    public String token;
+}
